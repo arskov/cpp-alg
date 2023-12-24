f8bd502b1f61168e0d9561b64a89cfd0c1dfbeb3
Arseni Kavalchuk <ars.kov@gmail.com>
2023-12-24 00:01:34 +0000

    Separate test file for a solution

--- test/test_gcd_of_strings.cpp
+++ test/test_gcd_of_strings.cpp
@@ -1,15 +1,10 @@
 #include "gcd_of_strings.h"
 #include <iostream>
 
-void test_gcd_of_strings()
+int main()
 {
     Solution s;
     std::string res = s.gcdOfStrings("ABCDABCD", "ABCD");
     std::cout << res << std::endl;
-}
-
-int main()
-{
-    test_gcd_of_strings();
     return 0;
 }
