089529df6bb53190ade1585c22ae8c7e6d2266fd
Arseni Kavalchuk <ars.kov@gmail.com>
2024-01-12 19:35:48 +0000

    Removed separate test files for solutions

--- src/gcd_of_strings.cpp
+++ src/gcd_of_strings.cpp
@@ -1,31 +1,43 @@
-#include "gcd_of_strings.h"
+#include <string>
 #include <iostream>
 
 using namespace std;
 
-string Solution::gcdOfStrings(string str1, string str2)
+class Solution
 {
-    if ((str1 + str2) != (str2 + str1))
-        return "";
-    string res{""};
-    for (size_t i = 1; i <= str2.length(); i++)
+public:
+    string gcdOfStrings(string str1, string str2)
     {
-        string_view pref(str2.data(), i);
-        if (is_devider(str1, pref) && is_devider(str2, pref))
+        if ((str1 + str2) != (str2 + str1))
+            return "";
+        string res{""};
+        for (size_t i = 1; i <= str2.length(); i++)
         {
-            res = pref;
+            string_view pref(str2.data(), i);
+            if (is_devider(str1, pref) && is_devider(str2, pref))
+            {
+                res = pref;
+            }
         }
+        return res;
     }
-    return res;
-}
 
-bool Solution::is_devider(string const &str, string_view const &pref)
-{
-    int times = str.length() / pref.length();
-    string tmp;
-    for (int i = 0; i < times; i++)
+private:
+    bool is_devider(string const &str, string_view const &pref)
     {
-        tmp += pref;
+        int times = str.length() / pref.length();
+        string tmp;
+        for (int i = 0; i < times; i++)
+        {
+            tmp += pref;
+        }
+        return str == tmp;
     }
-    return str == tmp;
+};
+
+int main() {
+    Solution s;
+    std::string res = s.gcdOfStrings("ABCDABCD", "ABCD");
+    std::cout << res << std::endl;
+    return 0;
 }
