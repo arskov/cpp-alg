5a89ab246df0aae1631aae10c193ae1e63f5f331
Arseni Kavalchuk <ars.kov@gmail.com>
2026-02-15 14:17:36 +0000

    More tests for Buy and Sell Stocks

--- src/lc_121_by_and_sell_stock_1.cpp
+++ src/lc_121_by_and_sell_stock_1.cpp
@@ -24,4 +24,24 @@ int main(int argc, char* argv[]) {
   vector<int> input {7,1,5,3,6,4};
   int res = s.maxProfit(input);
   assert(res == 5);
+
+  vector<int> input2 {7,6,4,3,1};
+  int res2 = s.maxProfit(input2);
+  assert(res2 == 0);
+
+  vector<int> input3 {1,2,3,4,5};
+  int res3 = s.maxProfit(input3);
+  assert(res3 == 4);
+
+  vector<int> input4 {2,4,1};
+  int res4 = s.maxProfit(input4);
+  assert(res4 == 2);
+
+  vector<int> input5 {3,3,5,0,0,3,1,4};
+  int res5 = s.maxProfit(input5);
+  assert(res5 == 4);
+
+  vector<int> input6 {2};
+  int res6 = s.maxProfit(input6);
+  assert(res6 == 0);
 }
