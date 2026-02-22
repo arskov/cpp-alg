bfe3e1b81b1e23d68535c4a1b981a8d22750c159
Arseni Kavalchuk <ars.kov@gmail.com>
2026-02-22 17:06:25 +0000

    Add valid palindrome template and tests

---
+++ src/lc_125_valid_palindrome.cpp
@@ -0,0 +1,42 @@
+#include <cassert>
+#include <cctype>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  bool isPalindrome(string s) {
+    int l = 0;
+    int r = s.size() - 1;
+    while (l < r) {
+      while (l < r && !isalnum((unsigned char)s[l]))
+        l++;
+      while (l < r && !isalnum((unsigned char)s[r]))
+        r++;
+      if (tolower((unsigned char)s[l]) != tolower((unsigned char)s[r]))
+        return false;
+      l++;
+      r++;
+    }
+    return false;
+  }
+};
+
+int main() {
+  Solution s;
+  bool res = s.isPalindrome("A man, a plan, a canal: Panama");
+  assert(res == true);
+  res = s.isPalindrome("race a car");
+  assert(res == false);
+  res = s.isPalindrome("");
+  assert(res == true);
+  res = s.isPalindrome(" ");
+  assert(res == true);
+  res = s.isPalindrome("0P");
+  assert(res == false);
+  res = s.isPalindrome("ab_a");
+  assert(res == true);
+  return 0;
+}
